e9453ebd06791cf4470bf0e5fb5c0bceee09b9df
Henrique Barcelos <henrique.barcelos@revmob.com>
2017-01-26 11:49:09 -0200

    Fixed implementation

--- todo-list.jsx
+++ todo-list.jsx
@@ -1,24 +1,46 @@
-import { Component } from 'react'
-import ReactDOM from 'react-dom'
-
-export default class TodoList extends Component {
+class TodoList extends React.Component {
   constructor() {
     super()
 
-    this.state.tasks = [{
-      done : false,
-      description : 'Test'
-    }]
+    this.state = {
+      tasks : [{
+        done : false,
+        description : 'Test'
+      }]
+    }
+  }
+
+  handleClick(taskDescription) {
+    const task = this.state.tasks.find(item => item.description === taskDescription)
+
+    if (task === undefined) return;
+
+    task.done = !task.done;
+
+    const newState = {
+      tasks : this.state.tasks.map(item => {
+        if (item.description === task.description) {
+          return task;
+        }
+        return item;
+      })
+    }
+
+    this.setState(newState)
   }
 
   render() {
     return (
-      <div class="wrapper">
-        <ul class="task-list">
+      <div className="wrapper">
+        <ul className="task-list">
           {
             this.state.tasks.map(function (task) {
-              return <li class={'task-list__task ' + task.done ? 'task-list__task--done' : ''}>task.description</li>
-            })
+              return <li key={task.description}
+                className={'task-list__task' + (task.done ? ' task-list__task--done' : '')}
+                onClick={this.handleClick.bind(this, task.description)}>
+                {task.description}
+                </li>
+            }, this)
           }
         </ul>
       </div>
